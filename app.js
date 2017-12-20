52e96fa8bc9ce1d4bd7db0a911484c52fb6073fb
Manjunatha D <mnjdvg@gwl.com>
2017-12-20 21:36:50 +0530

    Making GET and POST request to REST end-points using $http, and $resource

--- app.js
+++ app.js
@@ -1,38 +1,143 @@
-const app = angular.module("app", []).controller("controller", Controller);
+const app = angular
+  .module("app", ["ngResource"])
+  .controller("controller", Controller);
 
-function Controller($scope) {
-  $scope.searchTopic = "";
+Controller.$inject = ["$scope", "$http", "$resource"];
 
-  $scope.tableData = [
-    {
-      id: 1,
-      topic: "React",
-      contributions: 10,
-      tests: 5
+function Controller($scope, $http, $resource) {
+  // const URL = "https://dog.ceo/api/breeds/list";
+
+  // $http({ method: "GET", url: URL }).then(
+  //   response => {
+  //     $scope.breeds = response.data.message;
+  //     console.log(response);
+  //   },
+  //   error => {
+  //     alert("ERROR!!! ERROR!!!");
+  //   }
+  // );
+
+  // $resource(
+  //   URL,
+  //   {},
+  //   {
+  //     query: {
+  //       method: "GET"
+  //     }
+  //   }
+  // ).$promise.then(
+  //   response => {
+  //     $scope.breeds = response.data.message;
+  //     console.log(response);
+  //   },
+  //   error => {
+  //     alert("ERROR!!1");
+  //   }
+  // );
+
+  // const a = $resource(URL);
+
+  // const response = a.get();
+
+  // response.$promise.then(
+  //   response => {
+  //     console.log(response);
+  //     $scope.breeds = response.message;
+  //     console.log(response);
+  //   },
+  //   error => {
+  //     alert("ERROR!!1");
+  //   }
+  // );
+
+  // console.log(a);
+
+  const muleSoftBaseUrl =
+    "https://mocksvc.mulesoft.com/mocks/21bf111b-a6fb-4b78-8cb6-5066cd3dfd0b";
+
+  var PARAMS = {
+    GET_PENDING_TRANSFERS: {
+      gaId: "@gaId",
+      indId: "@indId"
     },
-    {
-      id: 2,
-      topic: "React-native",
-      contributions: 10,
-      tests: 5
-    },
-    {
-      id: 3,
-      topic: "Flux",
-      contributions: 10,
-      tests: 5
-    },
-    {
-      id: 4,
-      topic: "Flow",
-      contributions: 10,
-      tests: 5
-    },
-    {
-      id: 5,
-      topic: "Jest",
-      contributions: 10,
-      tests: 5
+    POST_CANCEL_REQUEST: {
+      gaId: "@gaId",
+      indId: "@indId",
+      evId: "@evId"
     }
-  ];
+  };
+
+  var URL = {
+    MOCK: {
+      GET_PENDING_TRANSFERS:
+        muleSoftBaseUrl + "/cancelTransfer/pendingTransfers",
+      POST_CANCEL_REQUEST: muleSoftBaseUrl + "/cancelTransfer/doCancel"
+    }
+  };
+
+  const getPendingTransfersUrl = URL.MOCK.GET_PENDING_TRANSFERS;
+  const postCancelRequestUrl = URL.MOCK.POST_CANCEL_REQUEST;
+
+  const factory = {
+    // get all pending transfers
+    getPendingTransfers: $resource(
+      getPendingTransfersUrl,
+      PARAMS.GET_PENDING_TRANSFERS,
+      {
+        query: {
+          method: "GET",
+          isArray: false,
+          withCredentials: false
+        }
+      }
+    ),
+
+    // post request to cancel a transfer
+    postCancelRequest: $resource(
+      postCancelRequestUrl,
+      PARAMS.POST_CANCEL_REQUEST,
+      {
+        query: {
+          method: "POST",
+          isArray: false,
+          withCredentials: false
+        }
+      }
+    )
+  };
+
+  const params = {
+    gaId: 123 - 01,
+    indId: 123
+  };
+
+  const postParams = {
+    gaId: 123 - 01,
+    indId: 123,
+    evId: 123
+  };
+
+  factory.getPendingTransfers
+    .query(params)
+    .$promise.then(
+      response => console.log("pending", response),
+      error => console.log(error)
+    );
+
+  factory.postCancelRequest
+    .query(postParams)
+    .$promise.then(
+      response => console.log("cancel via resource", response),
+      error => console.log(error)
+    );
+
+  $http({
+    method: "POST",
+    url: `${postCancelRequestUrl}?evId=${postParams.evId}&gaId=${
+      postParams.gaId
+    }&indId=${postParams.indId}`
+  }).then(
+    response => console.log("cancel via http", response),
+    error => console.log(error)
+  );
 }
