57b5c6e410ccc0a7e4740cd7675f78d4a50ef8c7
Manjunatha D <manjunathad13@gmail.com>
2018-05-15 12:39:39 +0530

    WIP: Search box

--- app.js
+++ app.js
@@ -1,143 +1,80 @@
-const app = angular
-  .module("app", ["ngResource"])
-  .controller("controller", Controller);
+const app = angular.module("app", ["ngResource"]).controller("controller", Controller);
 
 Controller.$inject = ["$scope", "$http", "$resource"];
 
 function Controller($scope, $http, $resource) {
-  // const URL = "https://dog.ceo/api/breeds/list";
+  $scope.searchText = "";
 
-  // $http({ method: "GET", url: URL }).then(
-  //   response => {
-  //     $scope.breeds = response.data.message;
-  //     console.log(response);
-  //   },
-  //   error => {
-  //     alert("ERROR!!! ERROR!!!");
-  //   }
-  // );
+  const SEPARATOR = " ";
 
-  // $resource(
-  //   URL,
-  //   {},
-  //   {
-  //     query: {
-  //       method: "GET"
-  //     }
-  //   }
-  // ).$promise.then(
-  //   response => {
-  //     $scope.breeds = response.data.message;
-  //     console.log(response);
-  //   },
-  //   error => {
-  //     alert("ERROR!!1");
-  //   }
-  // );
+  $scope.onSearchTextChange = function(searchText) {
+    const tokens = searchText.split(SEPARATOR);
 
-  // const a = $resource(URL);
-
-  // const response = a.get();
-
-  // response.$promise.then(
-  //   response => {
-  //     console.log(response);
-  //     $scope.breeds = response.message;
-  //     console.log(response);
-  //   },
-  //   error => {
-  //     alert("ERROR!!1");
-  //   }
-  // );
-
-  // console.log(a);
-
-  const muleSoftBaseUrl =
-    "https://mocksvc.mulesoft.com/mocks/21bf111b-a6fb-4b78-8cb6-5066cd3dfd0b";
-
-  var PARAMS = {
-    GET_PENDING_TRANSFERS: {
-      gaId: "@gaId",
-      indId: "@indId"
-    },
-    POST_CANCEL_REQUEST: {
-      gaId: "@gaId",
-      indId: "@indId",
-      evId: "@evId"
-    }
-  };
-
-  var URL = {
-    MOCK: {
-      GET_PENDING_TRANSFERS:
-        muleSoftBaseUrl + "/cancelTransfer/pendingTransfers",
-      POST_CANCEL_REQUEST: muleSoftBaseUrl + "/cancelTransfer/doCancel"
-    }
-  };
-
-  const getPendingTransfersUrl = URL.MOCK.GET_PENDING_TRANSFERS;
-  const postCancelRequestUrl = URL.MOCK.POST_CANCEL_REQUEST;
-
-  const factory = {
-    // get all pending transfers
-    getPendingTransfers: $resource(
-      getPendingTransfersUrl,
-      PARAMS.GET_PENDING_TRANSFERS,
-      {
-        query: {
-          method: "GET",
-          isArray: false,
-          withCredentials: false
-        }
-      }
-    ),
-
-    // post request to cancel a transfer
-    postCancelRequest: $resource(
-      postCancelRequestUrl,
-      PARAMS.POST_CANCEL_REQUEST,
-      {
-        query: {
-          method: "POST",
-          isArray: false,
-          withCredentials: false
-        }
-      }
-    )
-  };
-
-  const params = {
-    gaId: 123 - 01,
-    indId: 123
-  };
-
-  const postParams = {
-    gaId: 123 - 01,
-    indId: 123,
-    evId: 123
-  };
-
-  factory.getPendingTransfers
-    .query(params)
-    .$promise.then(
-      response => console.log("pending", response),
-      error => console.log(error)
+    const results = getSuggestions(tokens[tokens.length - 1]).then(
+      onGetSuggestionsSuccess,
+      onGetSuggestionsFailure
     );
 
-  factory.postCancelRequest
-    .query(postParams)
-    .$promise.then(
-      response => console.log("cancel via resource", response),
-      error => console.log(error)
-    );
+    function onGetSuggestionsSuccess(results) {
+      $scope.results = results;
+    }
 
-  $http({
-    method: "POST",
-    url: `${postCancelRequestUrl}?evId=${postParams.evId}&gaId=${
-      postParams.gaId
-    }&indId=${postParams.indId}`
-  }).then(
-    response => console.log("cancel via http", response),
-    error => console.log(error)
-  );
+    function onGetSuggestionsFailure(error) {
+      $scope.results = [];
+
+      alert(error);
+    }
+
+    $scope.updateInputBox = function(value) {
+      let tokens = $scope.searchText.split(SEPARATOR);
+
+      tokens[tokens.length - 1] = value + " ";
+
+      $scope.searchText = tokens.join();
+      $scope.results = [];
+
+      document.getElementById("search").focus();
+    };
+  };
 }
+
+// ================================= Mock Server Start =============================
+var FAILURE_COEFF = 10;
+var MAX_SERVER_LATENCY = 200;
+
+function getRandomBool(n) {
+  var maxRandomCoeff = 1000;
+  if (n > maxRandomCoeff) n = maxRandomCoeff;
+  return Math.floor(Math.random() * maxRandomCoeff) % n === 0;
+}
+
+function getSuggestions(text) {
+  var pre = "pre";
+  var post = "post";
+  var results = [];
+  if (getRandomBool(2)) {
+    results.push(pre + text);
+  }
+  if (getRandomBool(2)) {
+    results.push(text);
+  }
+  if (getRandomBool(2)) {
+    results.push(text + post);
+  }
+  if (getRandomBool(2)) {
+    results.push(pre + text + post);
+  }
+  return new Promise((resolve, reject) => {
+    var randomTimeout = Math.random() * MAX_SERVER_LATENCY;
+    setTimeout(() => {
+      if (getRandomBool(FAILURE_COEFF)) {
+        reject();
+      } else {
+        resolve(results);
+      }
+    }, randomTimeout);
+  });
+}
+// ================================= Mock Server End =============================
+
+// =============
